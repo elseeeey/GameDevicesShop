aaba5ba7c6171f97beef333877de3464738fbf62
Vlad <karnafelvlad@gmail.com>
2020-08-17 17:09:09 +0300

    Added new repositories and projects.

---
+++ GameDevicesStore/GameDevicesStore.DataAcces/Repository/Abstraction/IGenericRepository.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameDevicesStore.DataAcces.Repository.Abstraction
+{
+    public interface IGenericRepository<TEntity> where TEntity : class
+    {
+        void Create(TEntity entity);
+
+        void Delete(TEntity entity);
+
+        void Update(TEntity entity);
+
+        IEnumerable<TEntity> GetAll();
+        TEntity GetById(int id);
+    }
+}
---
+++ GameDevicesStore/GameDevicesStore.DataAcces/Repository/Implementation/EfRepository.cs
@@ -0,0 +1,46 @@
+﻿using GameDevicesStore.DataAcces.Repository.Abstraction;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameDevicesStore.DataAcces.Repository.Implementation
+{
+    public class EfRepository<TEntity> : IGenericRepository<TEntity> where TEntity : class
+    {
+        private readonly DbContext _context;
+        private readonly DbSet<TEntity> _set;
+
+        public EfRepository(DbContext context)
+        {
+            _context = context; 
+        }
+
+        public void Create(TEntity entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(TEntity entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<TEntity> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public TEntity GetById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(TEntity entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
